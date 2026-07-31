10a3eba8a72be54ef40d29cc0133db62a06f3b11
joellarramendia <joellarramendia@gmail.com>
2026-07-31 20:17:08 -0300

    Implement HomePage layout with CustomJumbotron, ProductsGrid, and CustomPagination components

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,6 +1,17 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+     <CustomJumbotron title="Todos los productos"/> 
+
+     <ProductsGrid products={products}/>
+
+     <CustomPagination totalPages={7}/>
+
+    </>
   )
 }
