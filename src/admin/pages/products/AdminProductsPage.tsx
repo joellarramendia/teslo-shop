54d18dfe614ff8940d57e30039d8b5c29aefe80d
joellarramendia <joellarramendia@gmail.com>
2026-08-03 18:13:55 -0300

    Add AdminProductsPage component for managing products with table and pagination

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,6 +1,61 @@
+import { AdminTitle } from "@/admin/components/AdminTitle"
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { Button } from "@/components/ui/button"
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
+import { PlusIcon } from "lucide-react"
+import { Link } from "react-router"
 
 export const AdminProductsPage = () => {
   return (
-    <div>AdminProductsPage</div>
+    <>
+
+      <div className="flex justify-between items-center">
+        <AdminTitle title="Productos" subtitle="Aquí puedes ver y administrar tus productos" />
+
+        <div>
+          <Link to='/admin/products/new'>
+            <Button className='p-5'>
+              <PlusIcon />
+              Nuevo producto
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+
+      <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-25">ID</TableHead>
+            <TableHead>Imagen</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Precio</TableHead>
+            <TableHead>Categoría</TableHead>
+            <TableHead>Inventario</TableHead>
+            <TableHead>Tallas</TableHead>
+            <TableHead className="text-right">Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>
+              <img src="https://placehold.co/250x250" alt="Producto" className="w-20 h-20 object-cover rounded-md" />
+            </TableCell>
+            <TableCell>Producto 1</TableCell>
+            <TableCell>$250.00</TableCell>
+            <TableCell>Categoría 1</TableCell>
+            <TableCell>100 stock</TableCell>
+            <TableCell>XS, S, L</TableCell>
+
+            <TableCell className="text-right">
+              <Link to={`/admin/products/t-shirt-teslo`}>Editar</Link>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <CustomPagination totalPages={10} />
+    </>
   )
 }
