c747a9c81b6aa6c5641dee92783486124b778b43
joellarramendia <joellarramendia@gmail.com>
2026-08-05 18:11:19 -0300

    feat: enhance auth store with checkAuthStatus action and update auth status management

--- src/auth/store/auth.store.ts
+++ src/auth/store/auth.store.ts
@@ -1,6 +1,7 @@
 import type { User } from '@/interfaces/user.interface'
 import { create } from 'zustand'
 import { loginAction } from '../actions/login.action'
+import { checkAuthAction } from '../actions/check-auth.action'
 
 type AuthStatus = 'authenticated' | 'not-authenticated' | 'checking'
 
@@ -14,6 +15,7 @@ type AuthState = {
     // Actions
     login: (email: string, password: string) => Promise<boolean>
     logout: () => void
+    checkAuthStatus: () => Promise<boolean>
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
@@ -27,12 +29,12 @@ export const useAuthStore = create<AuthState>()((set) => ({
             const data = await loginAction(email, password)
             localStorage.setItem('token', data.token)
 
-            set({user: data.user, token: data.token})
+            set({ user: data.user, token: data.token, authStatus: 'authenticated' })
 
             return true
         } catch (error) {
             localStorage.removeItem('token')
-            set({user: null, token: null})
+            set({ user: null, token: null, authStatus: 'not-authenticated' })
 
             return false
         }
@@ -40,7 +42,26 @@ export const useAuthStore = create<AuthState>()((set) => ({
 
     logout: () => {
         localStorage.removeItem('token')
-        set({user: null, token: null})
+        set({ user: null, token: null, authStatus: 'not-authenticated' })
+    },
+
+    checkAuthStatus: async () => {
+        try {
+            const { user, token } = await checkAuthAction()
+            set({
+                user: user,
+                token: token,
+                authStatus: 'authenticated'
+            })
+            return true
+        } catch (error) {
+            set({
+                user: undefined,
+                token: undefined,
+                authStatus: 'not-authenticated'
+            })
+            return false
+        }
     }
 }))
 
