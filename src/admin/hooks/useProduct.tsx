12129c2db5ffcfbf77c674490fbb3c95a7630dd1
joellarramendia <joellarramendia@gmail.com>
2026-08-06 18:38:13 -0300

    feat: enhance useProduct hook with mutation for product creation and updates

--- src/admin/hooks/useProduct.tsx
+++ src/admin/hooks/useProduct.tsx
@@ -1,15 +1,33 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { getProductByIdAction } from "../actions/get-product-by-id.action"
+import type { Product } from "@/interfaces/product.interface"
+import { createUpdateProductAction } from "../actions/create-update-prodcut.action"
 
 export const useProduct = (id: string) => {
+
+    const queryClient = useQueryClient()
+
     const query = useQuery({
-        queryKey: ['product', {id}],
+        queryKey: ['product', { id }],
         queryFn: () => getProductByIdAction(id),
         retry: false,
         staleTime: 1000 * 60 * 5
     })
 
+    const mutation = useMutation({
+        mutationFn: createUpdateProductAction,
+        onSuccess: (prodcut: Product) => {
+            // Invalidar cache
+            queryClient.invalidateQueries({queryKey: ['products']})
+            queryClient.invalidateQueries({queryKey: ['product', {id: prodcut.id}]})
+            // Actualizar queryData
+            queryClient.setQueryData(['products', {id: prodcut.id}], prodcut)
+        }
+    })
+
+
     return {
         ...query,
+        mutation
     }
 }
