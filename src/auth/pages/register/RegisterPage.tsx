977bf112da63d0aa5a0e41aa3ee42217a9dc594c
joellarramendia <joellarramendia@gmail.com>
2026-08-05 21:38:34 -0300

    feat: implement registration form handling and validation

--- src/auth/pages/register/RegisterPage.tsx
+++ src/auth/pages/register/RegisterPage.tsx
@@ -3,14 +3,44 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { CustomLogo } from "@/components/custom/CustomLogo"
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { useAuthStore } from "@/auth/store/auth.store"
+import { useState } from "react"
 
 export const RegisterPage = () => {
+
+  const navigate = useNavigate()
+
+  const { register } = useAuthStore()
+
+  const [isPosting, setIsPosting] = useState(false)
+
+  const handleRegister = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setIsPosting(true)
+
+    const formData = new FormData(e.target as HTMLFormElement)
+    const fullName = formData.get('fullName') as string
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    const isValid = await register(fullName, email, password)
+
+    if (isValid) {
+      navigate('/')
+      return
+    }
+
+
+
+    setIsPosting(false)
+  }
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleRegister}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <CustomLogo />
@@ -18,11 +48,11 @@ export const RegisterPage = () => {
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="fullName">Nombre Completo</Label>
-                <Input id="fullName" type="text" placeholder="Nombre Completo" required />
+                <Input id="fullName" name="fullName" type="text" placeholder="Nombre Completo" required />
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Correo</Label>
-                <Input id="email" type="email" placeholder="mail@example.com" required />
+                <Input id="email" name="email" type="email" placeholder="mail@example.com" required />
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
@@ -31,9 +61,9 @@ export const RegisterPage = () => {
                     Olvidaste tu contraseña?
                   </a>
                 </div>
-                <Input id="password" type="password" required placeholder="Contraseña" />
+                <Input id="password" name="password" type="password" required placeholder="Contraseña" />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Crear cuenta
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
