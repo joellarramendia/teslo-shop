4487ed5813f9be76ced270f37826f843f655d9b8
joellarramendia <joellarramendia@gmail.com>
2026-08-07 16:53:22 -0300

    feat: enhance ProductForm and AdminProductPage to support file uploads and display preview images

--- src/admin/pages/product/ui/ProductForm.tsx
+++ src/admin/pages/product/ui/ProductForm.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { useForm } from 'react-hook-form'
 import { AdminTitle } from "@/admin/components/AdminTitle";
@@ -14,19 +14,28 @@ interface Props {
     isPendig: boolean
 
     // Methods
-    onSubmit: (productLike: Partial<Product>) => Promise<void>
+    onSubmit: (productLike: Partial<Product> & {files?: File[]}) => Promise<void>
 }
 
 const availableSizes: Size[] = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
+interface FormInputs extends Product {
+    files?: File[]
+}
+
 
 export const ProductForm = ({ title, subTitle, product, onSubmit, isPendig }: Props) => {
     const [dragActive, setDragActive] = useState(false);
-    const { register, handleSubmit, formState: { errors }, getValues, setValue, watch } = useForm({
+    const { register, handleSubmit, formState: { errors }, getValues, setValue, watch } = useForm<FormInputs>({
         defaultValues: product
     })
 
     const labelInputRef = useRef<HTMLInputElement>(null)
+    const [files, setFiles] = useState<File[]>([])
+
+    useEffect(() => {
+        setFiles([])
+    }, [product])
 
     const selectedSizes = watch('sizes')
     const selectedTags = watch('tags')
@@ -83,12 +92,23 @@ export const ProductForm = ({ title, subTitle, product, onSubmit, isPendig }: Pr
         e.stopPropagation();
         setDragActive(false);
         const files = e.dataTransfer.files;
-        console.log(files);
+
+        if (!files) return
+
+        setFiles(prev => [...prev, ...Array.from(files)])
+
+        const currentFiles = getValues('files') || []
+        setValue('files', [...currentFiles, ...Array.from(files)])
     };
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
-        console.log(files);
+        if (!files) return
+
+        setFiles(prev => [...prev, ...Array.from(files)])
+
+        const currentFiles = getValues('files') || []
+        setValue('files', [...currentFiles, ...Array.from(files)])
     };
 
     return (
@@ -408,6 +428,27 @@ export const ProductForm = ({ title, subTitle, product, onSubmit, isPendig }: Pr
                                             </p>
                                         </div>
                                     ))}
+                                </div>
+                            </div>
+
+                            {/* Imagenes por cargar */}
+                            <div className={cn("mt-6 space-y-3", {
+                                'hidden': files.length === 0
+                            })}>
+                                <h3 className="text-sm font-medium text-slate-700">
+                                    Imágenes por cargar
+                                </h3>
+                                <div className="grid grid-cols-2 gap-3">
+                                    {
+                                        files.map((file, index) => (
+                                            <img
+                                                key={index}
+                                                src={URL.createObjectURL(file)}
+                                                alt="Product"
+                                                className="w-full h-full object-cover rounded-lg"
+                                            />
+                                        ))
+                                    }
                                 </div>
                             </div>
                         </div>
